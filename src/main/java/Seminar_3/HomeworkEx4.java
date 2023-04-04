d2f22565eda393614922dfccdb6c2e8d9c618e64
«Vlad» <fomichfomic@gmail.com>
2023-04-04 20:04:04 +0300

    Добавлены тексты заданий третьего семинара

--- src/main/java/Seminar_3/HomeworkEx4.java
+++ src/main/java/Seminar_3/HomeworkEx4.java
@@ -1,6 +1,11 @@
 package Seminar_3;
 import java.util.*;
 public class HomeworkEx4 {
+
+//    4*. Создать список типа ArrayList<String>. Поместить в него как строки, так и целые числа.
+//    Пройти по списку, найти и удалить целые числа.
+
+
     public static void main(String[] args) {
         List<String> list = new ArrayList<>(Arrays.asList("a","1","b","2","c","3"));
 
